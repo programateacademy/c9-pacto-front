3fdd49b5fe9dfcbe7748068f79cd52a050fed5ee
sewastian <sebastianrincon834@gmail.com>
2023-09-07 17:18:26 -0500

    update [core -> core.module.ts] imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './layout/header/header.component';
+import { HeaderComponent } from './layout/headerhome/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LoaderComponent } from './common/loader/loader.component';
 import { SkeletonComponent } from './common/skeleton/skeleton.component';
 import { ModalComponent } from './libs/modal.component';
+import { Header2Component } from './layout/headerauth/header2.component';
+import { Aside2Component } from './layout/aside2/aside2.component';
+import { AsideComponent } from './layout/aside/aside.component';
 
 
 
@@ -14,7 +17,10 @@ import { ModalComponent } from './libs/modal.component';
     FooterComponent,
     LoaderComponent,
     SkeletonComponent,
-    ModalComponent
+    ModalComponent,
+    Header2Component,
+    Aside2Component,
+    AsideComponent
   ],
   imports: [
     CommonModule
