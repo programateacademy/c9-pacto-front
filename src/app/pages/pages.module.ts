b72b2c72358ce50127526c3d625fbcc1502970da
sewastian <sebastianrincon834@gmail.com>
2023-09-06 23:19:34 -0500

    update [pages -> pages.module.ts]

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { ModalComponent } from './home/modal/modal.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
@@ -12,7 +11,6 @@ import { CoreModule } from '../core/core.module';
 @NgModule({
   declarations: [
     HomeComponent,
-    ModalComponent,
     LoginComponent,
     ProfileComponent,
     RegisterComponent
