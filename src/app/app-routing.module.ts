867c56f746c72e6c6130791f07c42c40b640f15a
John9135 <palacioalexander5@gmail.com>
2023-09-08 22:20:39 -0500

    update creation de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  {path: 'profile', component:ProfileComponent},
-  {path: 'register', component:RegisterComponent}
+  { path: 'profile', component: ProfileComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
