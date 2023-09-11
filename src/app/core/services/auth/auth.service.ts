312566fd43f5247089d80ec6f8074c0b059df48a
Brayan <brayantandap@gmail.com>
2023-09-10 23:05:05 -0500

    'HttpClient' & 'Router' for data storage > auth.service

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // URL solicitudes
+  private URL = ''
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 }
