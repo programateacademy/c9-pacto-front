31106249fe9bd9a1c33b89f22aa489d81e9996a8
John9135 <palacioalexander5@gmail.com>
2023-09-12 18:15:25 -0500

    feat: Creating folders and files to add departments and their cities to the register form

---
+++ src/app/core/services/formulario/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { asyncData } from 'src/testing/async-observable-helpers.config';
+import { capitales } from './capitales';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  constructor(private http: HttpClient) { }
+
+  getforms() {
+    return asyncData(capitales)
+
+  }
+}
---
+++ src/app/pages/formulario/formulario.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../core/services/api.service';
+import { capitales } from '../../core/services/formulario/capitales';
+
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {
+  capitalesdata = capitales;
+  departamentosUnicos: string[] = [];
+  municipiosUnicos: string[] = [];
+
+
+  constructor(private apiservice: ApiService) { }
+
+  ngOnInit(): void {
+    // Lógica para obtener listas de departamentos y municipios únicos
+    this.departamentosUnicos = this.obtenerDepartamentosUnicos();
+    this.municipiosUnicos = this.obtenerMunicipiosUnicos();
+  }
+
+  private obtenerDepartamentosUnicos(): string[] {
+    const departamentos: string[] = this.capitalesdata.map(capital => capital.departamento);
+    return departamentos.filter((departamento, index, self) => self.indexOf(departamento) === index);
+  }
+
+  private obtenerMunicipiosUnicos(): string[] {
+    const municipios: string[] = this.capitalesdata.map(capital => capital.municipio);
+    return municipios.filter((municipio, index, self) => self.indexOf(municipio) === index);
+  }
+
+}
