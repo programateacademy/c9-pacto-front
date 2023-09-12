e152b8e3f90b3f8d2bcae280b51a7d26de68dc3f
sewastian <sebastianrincon834@gmail.com>
2023-09-12 12:52:39 -0500

    feat [services -> modal.component.ts]

--- src/app/core/libs/modal.component.ts
+++ src/app/core/libs/modal.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { SwitchService } from '../services/modal/switch.service';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
+
+  constructor(private modalSS: SwitchService){}
+
+  ngOnInit():void{
+  }
+
+
+  closeModal(){
+
+    this.modalSS.$modal.emit(false)
+  }
 
 }
