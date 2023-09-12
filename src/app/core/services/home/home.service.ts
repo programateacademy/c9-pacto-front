21fa044bd02e7cda599248255d21a5d4d80f8bfd
sewastian <sebastianrincon834@gmail.com>
2023-09-12 12:53:22 -0500

    feat [services -> home.services.ts]

--- src/app/core/services/home/home.service.ts
+++ src/app/core/services/home/home.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Home } from 'src/app/models/item';
 @Injectable({
   providedIn: 'root'
 })
-export class HomeService {
+export class ForoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTask(url: string): Observable<Home[]> {
+    return this.http.get<Home[]>(url);
+  }
+
+  getUsernameById(userId:String): Observable<string>{
+    const userUrl = `https://pooforoapi.onrender.com/poofo/${userId}`
+    return this.http.get<string>(userUrl)
+  }
 }
