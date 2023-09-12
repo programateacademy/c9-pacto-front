5c7c3b73581a5425f5eac78ebd2f9c088aac5242
sewastian <sebastianrincon834@gmail.com>
2023-09-12 12:54:09 -0500

    feat [services -> switch.services.ts]

---
+++ src/app/core/services/modal/switch.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SwitchService {
+
+  constructor() { }
+  $modal = new EventEmitter<any>()
+}
