490136d4289950047be1f80ca3a4b1bc1a310cbf
nicolas <jaljordan77@gmail.com>
2023-09-14 17:49:02 -0500

    feat->[home.component.ts]

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
@@ -6,14 +6,45 @@ import { AuthService } from '../../services/auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-
 export class HeaderComponent {
-
   userId: string | null = null;
+  isDropdownOpen = false; // Variable para controlar la visibilidad del menú desplegable
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.userId = this.authService.getLoggedInUserId();
+  }
+
+  // Método para alternar la visibilidad del menú
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+
+    // Obtener el elemento del menú desplegable
+    const dropdownMenu = this.el.nativeElement.querySelector('#dropdownMenu');
+
+    if (this.isDropdownOpen) {
+      // Mostrar el menú desplegable
+      this.renderer.removeClass(dropdownMenu, 'hidden');
+    } else {
+      // Ocultar el menú desplegable
+      this.renderer.addClass(dropdownMenu, 'hidden');
+    }
+  }
+
+  // Método para cerrar el menú cuando se hace clic en cualquier lugar fuera de él
+  closeDropdown(event: MouseEvent) {
+    if (!this.el.nativeElement.contains(event.target)) {
+      this.isDropdownOpen = false;
+      const dropdownMenu = this.el.nativeElement.querySelector('#dropdownMenu');
+      this.renderer.addClass(dropdownMenu, 'hidden');
+    }
+  }
+
+  // Método para cerrar el menú cuando se hace clic en el botón "X"
+  closeDropdownWithButton() {
+    this.isDropdownOpen = false;
+    const dropdownMenu = this.el.nativeElement.querySelector('#dropdownMenu');
+    this.renderer.addClass(dropdownMenu, 'hidden');
   }
 }
