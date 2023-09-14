746b17f5f7f3b183864b06b090bf5e5fd0ec35fb
sewastian <sebastianrincon834@gmail.com>
2023-09-14 15:02:58 -0500

    update [app -> app-routing.module.ts] import Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './errors/page-not-found.component';
+import { authGuard } from 'src/guard/auth.guard';
 
 const routes: Routes = [
 
-  { path: 'profile/:id', component: ProfileComponent },
+  { path: 'profile/:id', component: ProfileComponent, canActivate:[authGuard] },
   { path: 'register', component: RegisterComponent },
   { path: '', component: LoginComponent },
-  { path: 'home/:id', component: HomeComponent },
+  { path: 'home/:id', component: HomeComponent, canActivate:[authGuard] },
 
   // Ruta wildcard para manejar errores 404
   {path:'**', component:PageNotFoundComponent}
