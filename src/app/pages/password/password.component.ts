a8ba2169d3e57e132fae2f6d475a220b4dff3e35
nicolas <jaljordan77@gmail.com>
2023-09-22 13:59:26 -0500

    update->[password.component.ts]

--- src/app/pages/password/password.component.ts
+++ src/app/pages/password/password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-password',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./password.component.css']
 })
 export class PasswordComponent {
+  passwordForm: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.passwordForm = this.formBuilder.group({
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  updatePassword() {
+    if (this.passwordForm.valid) {
+      const newPassword = this.passwordForm.value.newPassword;
+
+      this.authService.changePassword(newPassword)
+        .subscribe(
+          (response) => {
+            // Manejar la respuesta exitosa aquí si es necesario
+            console.log('Contraseña actualizada con éxito', response);
+
+            // Luego, puedes borrar los campos de contraseña
+            this.passwordForm.reset();
+          },
+          (error) => {
+            // Manejar el error aquí si es necesario
+            console.error('Error al actualizar la contraseña', error);
+          }
+        );
+    }
+  }
 }
