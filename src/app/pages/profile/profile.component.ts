c9c588c4d77d0b0e611f2ab67c04b58ebc183c68
Brayan <brayantandap@gmail.com>
2023-09-22 17:58:47 -0500

    Updated code and data user

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { ProfileService } from '../../core/services/profile/profile.service';
 import { AuthService } from '../../core/services/auth/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { User, Home, Comment } from 'src/app/models/item';
+import { User, Home } from 'src/app/models/item';
 import { SwitchUserService } from 'src/app/core/services/modalUs/switch-user.service';
 import { forkJoin } from 'rxjs';
 import { ForoService } from 'src/app/core/services/home/home.service';
-import { InteractionService } from 'src/app/core/services/interactions/interaction.service';
-import { CommentsService } from 'src/app/core/services/comments/comments.service';
 import { Renderer2 } from '@angular/core';
 
 @Component({
@@ -20,33 +18,24 @@ export class ProfileComponent {
   user: User | null = null;
   users: User[] = [];
   public listpublications: Home[] = [];
-  public comments: Comment[] = [];
-  likedPublications: { [key: string]: boolean } = {};
-  commentContent?: string;
   imageURL: string = '';
-  isCommentModalVisible: boolean = false;
-
   publicationId: string;
+  isModalVisible !: boolean;
 
-  liked: boolean = false;
   constructor(private modalUser: SwitchUserService,
-    private router: Router,
     private route: ActivatedRoute,
     private ProfileService: ProfileService,
     private authService: AuthService,
-    private interactionService: InteractionService,
-    private commentService: CommentsService,
     private foroService: ForoService,
     private renderer: Renderer2) { this.publicationId = ''; }
 
-  isModalVisible !: boolean;
 
   ngOnInit(): void {
-
     this.modalUser.$modal.subscribe((valu) => { this.isModalVisible = valu })
     this.loadData();
     this.dataUser();
   }
+
   showContentOne = true;
   showContentTwo = false;
   toggleContenido() {
@@ -59,6 +48,7 @@ export class ProfileComponent {
     this.showOptions = !this.showOptions;
 
   }
+
   dataUser() {
     // Obtiene el ID del usuario logueado desde el servicio de autenticación
     const loggedInUserId = this.authService.getLoggedInUserId();
@@ -76,8 +66,7 @@ export class ProfileComponent {
           // Obtiene los datos del usuario desde el servicio de perfil
           this.ProfileService.getUser(id).subscribe(data => {
             this.user = data;
-            console.log(data)
-
+            console.log('Data User prfile', data)
           });
         } else {
           console.error('Error ids diferentes no coinciden')
@@ -87,13 +76,12 @@ export class ProfileComponent {
     }
   }
 
-
   public loadData() {
     this.foroService.getTask('publictpacto/')
       .subscribe((data: Home[]) => {
         const requests = data.map(publication => this.foroService.getUsernameById(publication.user));
         const userId = this.authService.getLoggedInUserId();
-        console.log('Inicio de likePublication', userId);
+
         forkJoin(requests).subscribe((responses: any[]) => {
 
           const usernames = responses.map(response => response?.userName);
@@ -102,31 +90,17 @@ export class ProfileComponent {
           // Filtra las publicaciones para obtener solo las del usuario logeado
           data = data.filter(publication => publication.user === userId);
 
-          // Map the likes information
-          const likes = data.map(publication => {
-            return {
-              publicationId: publication._id,
-              likedBy: publication.likes
-            };
-          });
-
-
           this.listpublications = data.map((publication, index) => ({
             ...publication,
             username: usernames[index],
             userimg: userimgs[index],
-            liked: this.likedPublications[publication._id] === true,
-            likes: likes.find(like => like.publicationId === publication._id)?.likedBy || []
-          })); console.log('Likes por publicaicon', likes)
-
+          }));
+          console.log('Data Publi prfile', data)
         });
       });
   }
 
-
-
   openModal() {
-
     this.isModalVisible = true
   }
 }
