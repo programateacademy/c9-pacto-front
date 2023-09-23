ae5a6b1a735dd9ac68d0c5f316c9c15eb7cde8ff
Brayan <brayantandap@gmail.com>
2023-09-23 16:00:28 -0500

    Added function to issue userData

--- src/app/core/services/modalUs/switch-user.service.ts
+++ src/app/core/services/modalUs/switch-user.service.ts
@@ -6,5 +6,13 @@ import { Injectable, EventEmitter } from '@angular/core';
 export class SwitchUserService {
 
   constructor() { }
-  $modal = new EventEmitter<any>()
+  $modal = new EventEmitter<any>();
+  userData = new EventEmitter<any>();
+
+  sendUserData(user: any): void {
+    // Emitir datos del user al componente modal
+    this.userData.emit(user);
+    console.log('dataUser switchService: ', user)
+  }
+
 }
