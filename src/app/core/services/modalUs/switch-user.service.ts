7cb3ef220abf8186c819309c3a32b883ff8fa516
Brayan <brayantandap@gmail.com>
2023-09-24 16:44:03 -0500

    Added function request 'updateUser'

--- src/app/core/services/modalUs/switch-user.service.ts
+++ src/app/core/services/modalUs/switch-user.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { enviroment } from 'src/environments/environment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwitchUserService {
+  private URL = enviroment.apiUrl
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   $modal = new EventEmitter<any>();
   userData = new EventEmitter<any>();
 
@@ -13,6 +17,12 @@ export class SwitchUserService {
     // Emitir datos del user al componente modal
     this.userData.emit(user);
     console.log('dataUser switchService: ', user)
+  }
+
+  updateUser(userId: string, userData: any): Observable<any> {
+    const url = `${this.URL}users/update/${userId}`;
+    console.log('userData switchUser', userData)
+    return this.http.put(url, userData);
   }
 
 }
