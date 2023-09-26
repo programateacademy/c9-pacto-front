dc6abe1187c8a001da6262d72881bf5ba7686471
JMCardenass <JMCardenas1807@gmail.com>
2023-09-26 14:01:05 -0500

    feat: add skeleton comments

---
+++ src/app/core/common/skeleton-coments/skeleton-coments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-coments',
+  templateUrl: './skeleton-coments.component.html',
+  styleUrls: ['./skeleton-coments.component.css']
+})
+export class SkeletonComentsComponent {
+
+}
